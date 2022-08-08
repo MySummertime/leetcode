a22d7a836cb0f9857c6b1461c1d507b431463e7c
frances.jiang <frances.bh.jiang@gmail.com>
2022-08-08 18:14:08 +0800

    add solution: divide & conquer

--- 4.寻找两个正序数组的中位数.cpp
+++ 4.寻找两个正序数组的中位数.cpp
@@ -7,6 +7,51 @@ using std::vector;
 
 class Solution {
 public:
+    // (divide & conquer) & binary search
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int total = nums1.size() + nums2.size();
+        // scale is even
+        if ((total & 1) == 0) {
+            int left = find(nums1, 0, nums2, 0, total >> 1);
+            int right = find(nums1, 0, nums2, 0, ((total >> 1) + 1));
+            return (left + right) / 2.0;
+        }
+        // scale is odd
+        return find(nums1, 0, nums2, 0, (total >> 1) + 1);
+    }
+
+private:
+    // i: the first idx of v1's valid scope
+    // j: the first idx of v2's valid scope
+    // k: the valid scope
+    int find(vector<int> nums1, int i, vector<int> nums2, int j, int k) {
+        int len1 = nums1.size();    int len2 = nums2.size();
+        // To simplify border processing operations,
+        // make the valid scale of v1 <= the scale of v2
+        if (len1 - i > len2 - j) {
+            return find(nums2, j, nums1, i, k);
+        }
+        if (i >= len1) {
+            return nums2[j + k - 1];
+        }
+        if (k != 1) {
+            int m1 = std::min(i + (k >> 1), len1);
+            int m2 = j + k - (k >> 1);
+            if (nums1[m1 - 1] > nums2[m2 - 1]) {
+                return find(nums1, i, nums2, m2, k - (m2 - j));
+            }
+            else {
+                return find(nums1, m1, nums2, j, k - (m1 - i));
+            }
+        }
+        return std::min(nums1[i], nums2[j]);
+    }
+};
+
+
+/**
+class Solution {
+public:
     // double pointers
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int m = nums1.size();    int n = nums2.size();
@@ -40,4 +85,4 @@ public:
         return (double)(std::max(nums1_left_max, nums2_left_max) + std::min(nums1_right_min, nums2_right_min)) / 2;
     }
 };
-
+*/
