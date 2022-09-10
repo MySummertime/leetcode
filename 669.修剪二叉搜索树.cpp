173a08aa252dde31e02c7183aa6512fb834848bd
frances.jiang <frances.bh.jiang@gmail.com>
2022-09-10 10:03:54 +0800

    modify: add a traverse solution

--- 669.修剪二叉搜索树.cpp
+++ 669.修剪二叉搜索树.cpp
@@ -10,6 +10,7 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+/*
 class Solution {
 public:
     // pre-order
@@ -29,5 +30,46 @@ private:
             return dfs(root->right, l, r);
         }
         return dfs(root->left, l, r);
+    }
+};
+*/
+
+class Solution {
+public:
+    // bfs
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        return bfs(root, low, high);
+    }
+
+private:
+    TreeNode* bfs(TreeNode* root, int l, int r) {
+        while (root && (root->val < l || root->val > r)) {
+            if (root->val < l) {
+                root = root->right;
+            }
+            else {
+                root = root->left;
+            }
+        }
+        if (!root)  return nullptr;
+        // traverse the left subtree
+        for (auto node = root; node->left; ) {
+            if (node->left->val < l) {
+                node->left = node->left->right;
+            }
+            else {
+                node = node->left;
+            }
+        }
+        // traverse the right subtree
+        for (auto node = root; node->right; ) {
+            if (node->right->val > r) {
+                node->right = node->right->left;
+            }
+            else {
+                node = node->right;
+            }
+        }
+        return root;
     }
 };
