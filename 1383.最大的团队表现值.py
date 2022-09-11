dc2b00706bf6a284730d6cbc1930aeca9e62500b
frances.jiang <frances.bh.jiang@gmail.com>
2022-09-11 11:44:55 +0800

    add 1383.Maximum Performance of a Team.py

---
+++ 1383.最大的团队表现值.py
@@ -0,0 +1,17 @@
+
+import heapq
+from typing import List
+
+class Solution:
+    # priority queue
+    def maxPerformance(self, n: int, speed: List[int], efficiency: List[int], k: int) -> int:
+        vec = []
+        ans = ssum = 0
+        # customized sort: efficiency descending
+        for e, s in sorted(zip(efficiency, speed), reverse=1):
+            heapq.heappush(vec, s)
+            ssum += s
+            if len(vec) > k:
+                ssum -= heapq.heappop(vec)
+            ans = max(ans, ssum * e)
+        return ans % (10 ** 9 + 7)
