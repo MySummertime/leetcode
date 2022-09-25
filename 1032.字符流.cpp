efe76011d69e97343928138c88218c502b102814
frances.jiang <frances.bh.jiang@gmail.com>
2022-09-25 08:57:29 +0800

    add a new solution to 1032

--- 1032.字符流.cpp
+++ 1032.字符流.cpp
@@ -10,7 +10,7 @@ using std::string;
  * StreamChecker* obj = new StreamChecker(words);
  * bool param_1 = obj->query(letter);
  */
-
+/*
 class Trie {
 public:
     bool is_leaf;
@@ -63,6 +63,70 @@ public:
 
 private:
     Trie root;
+    vector<char> waiting;
+    int scale;
+};
+*/
+
+/**
+ * Your StreamChecker object will be instantiated and called as such:
+ * StreamChecker* obj = new StreamChecker(words);
+ * bool param_1 = obj->query(letter);
+ */
+class Trie {
+public:
+    bool is_end;
+    Trie* children[26];
+
+public:
+    Trie(): is_end(false) {
+        memset(children, 0, sizeof(children));
+    }
+
+    void insert(string s, bool reverse = false) {
+        Trie* node = this;
+        for (int len = s.length(), i = (reverse) ? len - 1 : 0; 0 <= i && i < len; i += (reverse) ? -1 : 1) {
+            if (!node->children[s[i] - 'a']) {
+                node->children[s[i] - 'a'] = new Trie();
+            }
+            node = node->children[s[i] - 'a'];
+        }
+        node->is_end = true;
+    }
+};
+
+
+class StreamChecker {
+public:
+    StreamChecker(vector<string>& words) {
+        scale = 0;
+        waiting.clear();
+        for (const string& s : words) {
+            tree.insert(s, true);
+            scale = scale > s.length() ? scale : s.length();
+        }
+    }
+
+    bool query(char letter) {
+        waiting.insert(waiting.begin(), letter);
+        if (waiting.size() > scale) {
+            waiting.pop_back();
+        }
+        Trie* node = &tree;
+        for (auto it = waiting.begin(); it != waiting.end(); ++it) {
+            if (node->is_end) {
+                return true;
+            }
+            if (!node->children[*it - 'a']) {
+                return false;
+            }
+            node = node->children[*it - 'a'];
+        }
+        return node->is_end;
+    }
+
+private:
+    Trie tree;
     vector<char> waiting;
     int scale;
 };
