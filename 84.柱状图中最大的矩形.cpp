d744f78317815942eecd9cd108d338bda511cb01
frances.jiang <frances.bh.jiang@gmail.com>
2022-10-21 20:02:17 +0800

    add a new solution to 84

--- 84.柱状图中最大的矩形.cpp
+++ 84.柱状图中最大的矩形.cpp
@@ -4,6 +4,42 @@
 using std::vector;
 using std::stack;
 
+
+class Solution {
+public:
+    // monotinic stack
+    int largestRectangleArea(vector<int>& heights) {
+        int len = heights.size();
+        if (len == 1)   return heights[0];
+        int ans = 0;
+        stack<int> stk;
+        for (int i = 0; i < len; ++i) {
+            while (!stk.empty() && heights[i] < heights[stk.top()]) {
+                int h = heights[stk.top()];
+                stk.pop();
+                int w = i;
+                if (!stk.empty()) {
+                    w = i - stk.top() - 1;
+                }
+                ans = std::max(ans, h * w);
+            }
+            stk.push(i);
+        }
+        while (!stk.empty()) {
+            int h = heights[stk.top()];
+            stk.pop();
+            int w = len;
+            if (!stk.empty()) {
+                w = len - stk.top() - 1;
+            }
+            ans = std::max(ans, h * w);
+        }
+        return ans;
+    }
+};
+
+
+/*
 class Solution {
 public:
     // monotinic stack
@@ -33,3 +69,4 @@ public:
         return ans;
     }
 };
+*/
