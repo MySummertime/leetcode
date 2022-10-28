78a4ea80ca80808997239955839897de67484d33
frances.jiang <frances.bh.jiang@gmail.com>
2022-10-28 21:09:31 +0800

    add 907.Sum of Subarray Minimums.cpp

---
+++ 907.子数组的最小值之和.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <stack>
+
+using std::vector;
+using std::stack;
+
+
+class Solution {
+public:
+    // monotonic stack
+    int sumSubarrayMins(vector<int>& arr) {
+        arr.push_back(-1);
+        long ans = 0L;
+        stack<int> stk;
+        stk.push(-1);
+        for (int r = 0; r < arr.size(); ++r) {
+            while (stk.size() > 1 && arr[r] <= arr[stk.top()]) {
+                int l = stk.top();
+                stk.pop();
+                ans += (long) arr[l] * (l - stk.top()) * (r - l);
+            }
+            stk.push(r);
+        }
+        return ans % MOD;
+    }
+
+private:
+    static constexpr int MOD = 1e9 + 7;
+};
