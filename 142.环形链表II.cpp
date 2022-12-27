c8a2953fddf97db5b3884f2d75221c2c0e71195a
frances.jiang <frances.bh.jiang@gmail.com>
2022-12-27 14:00:34 +0800

    modified 142

--- 142.环形链表II.cpp
+++ 142.环形链表II.cpp
@@ -7,6 +7,7 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
 };
 
+/*
 class Solution {
 public:
     // fast & slow pointers
@@ -28,5 +29,26 @@ public:
             fast = fast->next;
         }
         return slow;
+    }
+};
+*/
+
+class Solution {
+public:
+    // two pointers
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (fast == slow) {
+                while (slow != head) {
+                    slow = slow->next;
+                    head = head->next;
+                }
+                return slow;
+            }
+        }
+        return nullptr;
     }
 };
