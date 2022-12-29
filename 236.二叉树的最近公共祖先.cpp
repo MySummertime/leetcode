28a4d11f79398a18b0c7e7336f2393b7c1fcf1a5
frances.jiang <frances.bh.jiang@gmail.com>
2022-12-29 12:44:51 +0800

    add 236.Lowest Common Ancestor of a Binary Tree.cpp

---
+++ 236.二叉树的最近公共祖先.cpp
@@ -0,0 +1,24 @@
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution {
+public:
+    // post-order dfs
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || !p || !q)  return nullptr;
+        if (root == p || root == q) return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if (left && right)  return root;
+        if (left)   return left;
+        return right;
+    }
+};
