6dd9a264d7ec362bd1f8899a22809600674d756b
frances.jiang <frances.bh.jiang@gmail.com>
2023-01-10 19:05:23 +0800

    modified 100

--- 100.相同的树.cpp
+++ 100.相同的树.cpp
@@ -1,5 +1,7 @@
+#include <stack>
 #include <queue>
 
+using std::stack;
 using std::queue;
 
 
@@ -13,39 +15,71 @@ struct TreeNode {
 };
 
 
-/*
-class Solution {
+class Solution100_1 {
 public:
-    // recursion
+    // postorder dfs recursion
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (!p && !q)   return true;
-        if (!p || !q)   return false;
-        return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-    }
-};
-*/
-
-
-class Solution {
-public:
-    // traversion
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        return traversion(p, q);
+        return dfs(p, q);
     }
 
 private:
-    bool traversion(TreeNode* p, TreeNode* q) {
+    bool dfs(TreeNode* _p, TreeNode* _q) {
+        if (!_p && !_q) return true;
+        else if (!_p || !_q || _p->val != _q->val)  return false;
+        bool left = dfs(_p->left, _q->left);    // left
+        bool right = dfs(_p->right, _q->right); // right
+        return left && right;
+    }
+};
+
+
+class Solution100_2 {
+public:
+    // postorder iteration
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q)   return true;
+        else if (!p || !q || p->val != q->val)  return false;
+        stack<TreeNode*> stk;
+        stk.push(q);
+        stk.push(p);
+        while (!stk.empty()) {
+            TreeNode* l = stk.top();
+            stk.pop();
+            TreeNode* r = stk.top();
+            stk.pop();
+            if (!l && !r)   continue;
+            else if (!l || !r || l->val != r->val)  return false;
+            stk.push(l->right); // right
+            stk.push(r->right);
+            stk.push(l->left);  // left
+            stk.push(r->left);
+        }
+        return true;
+    }
+};
+
+
+
+class Solution100_3 {
+public:
+    // bfs traversal
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q)   return true;
+        else if (!p || !q || p->val != q->val)  return false;
         queue<TreeNode*> que;
         que.push(p);
         que.push(q);
         while (!que.empty()) {
-            TreeNode* l = que.front();  que.pop();
-            TreeNode* r = que.front();  que.pop();
+            TreeNode* l = que.front();
+            que.pop();
+            TreeNode* r = que.front();
+            que.pop();
             if (!l && !r)   continue;
-            if (!l || !r)   return false;
-            if (l->val != r->val)   return false;
-            que.push(l->left);  que.push(r->left);
-            que.push(l->right); que.push(r->right);
+            else if (!l || !r || l->val != r->val)  return false;
+            que.push(l->left);  // left
+            que.push(r->left);
+            que.push(l->right); // right
+            que.push(r->right);
         }
         return true;
     }
