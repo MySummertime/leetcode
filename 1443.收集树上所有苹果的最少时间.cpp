50b1355e7503726f18c69b3a4e15894ce923e5ce
frances.jiang <frances.bh.jiang@gmail.com>
2023-01-12 21:39:03 +0800

    modified 1443.Minimum Time to Collect All Apples in a Tree.cpp

--- 1443.收集树上所有苹果的最少时间.cpp
+++ 1443.收集树上所有苹果的最少时间.cpp
@@ -7,27 +7,37 @@ using std::unordered_map;
 
 class Solution {
 public:
-    // dfs recursion
+    // dfs recursion && backtrack
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
         adj_list.resize(n);
         for (const vector<int> &e : edges) {
             adj_list[e[0]].push_back(e[1]);
             adj_list[e[1]].push_back(e[0]);
         }
-        return dfs(hasApple, 0, 0, -1) * 2;
+        return 2 * dfs(hasApple, 0, 0, -1);
     }
 
 private:
-    int dfs(const vector<bool>& hasApple, int vertice, int d, int prev) {
-        int ans = 0, temp;
-        for (int &i : adj_list[vertice]) {
-            // make sure the edge is not a circuit
+    int dfs(const vector<bool>& hasApple, int idx, int dis, int prev) {
+        int ans = 0;
+        // traverse the sub-tree of vertice at 'idx'
+        for (const int& i : adj_list[idx]) {
+            // in case that we trace back to the vertice at 'idx'(cycle)
             if (i != prev) {
-                temp = dfs(hasApple, i, d + 1, vertice);
-                if (temp)   ans += temp - d;
+                int cnt = dfs(hasApple, i, dis + 1, idx);
+                // if we found some apples in the sub-tree
+                // we add the newly accumulated distance into the answer before backtracking the distance
+                if (cnt)    ans += cnt - dis;
             }
         }
-        return ans || hasApple[vertice] ? ans + d : 0;
+        // if some apples were found in this round,
+        // return the formerly accumulated distance + ans.
+        // if no apple was found in this round,
+        // simply return the formerly accumulated distance.
+        if (ans || hasApple[idx]) {
+            return ans + dis;
+        }
+        return 0;
     }
 
 private:
