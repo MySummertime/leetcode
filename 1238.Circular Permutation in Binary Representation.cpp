8b00790b8e782e3852ab6911657a62abb8e0e906
frances.jiang <frances.bh.jiang@gmail.com>
2023-02-23 19:00:55 +0800

    add 1238.Circular Permutation in Binary Representation.cpp

---
+++ 1238.Circular Permutation in Binary Representation.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+using std::vector;
+
+
+class Solution {
+public:
+    vector<int> circularPermutation(int n, int start) {
+        vector<int> res;
+        for (int i = 0; i < 1 << n; ++i)
+            res.push_back(start ^ i ^ i >> 1);
+        return res;
+    }
+};
