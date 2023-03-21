29ebf40c67e8f5ccaf2be7b1705918e71db55e95
frances.jiang <frances.bh.jiang@gmail.com>
2023-03-21 21:20:51 +0800

    add 2469.Convert the Temperature.cpp

---
+++ 2469.Convert the Temperature.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+using std::vector;
+
+
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        return {celsius + 273.15, celsius * 1.80 + 32.00};
+    }
+};
