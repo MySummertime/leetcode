221a7c0be93b000046ae26a25d01b5d0fa4d3403
frances.jiang <frances.bh.jiang@gmail.com>
2023-03-28 15:17:56 +0800

    modified 983

--- 983.Minimum Cost for Tickets.cpp
+++ 983.Minimum Cost for Tickets.cpp
@@ -12,16 +12,19 @@ public:
         vector<int> dp(len + 1, INT32_MAX);
         dp[0] = 0;
         for (int i = 1, j; i <= len; ++i) {
+            // 1-day pass
             dp[i] = dp[i - 1] + costs[0];
             j = i - 1;
             while (j >= 0 && days[i - 1] - days[j] < 7) {
                 --j;
             }
+            // 7-days pass
             dp[i] = std::min(dp[i], dp[j + 1] + costs[1]);
             j = i - 1;
             while (j >= 0 && days[i - 1] - days[j] < 30) {
                 --j;
             }
+            // 30-days pass
             dp[i] = std::min(dp[i], dp[j + 1] + costs[2]);
         }
         return dp[len];
